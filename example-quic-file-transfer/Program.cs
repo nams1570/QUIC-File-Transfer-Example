3cfae91f5d6a8002bb66b90ed99d4d910ab3b834
Aman Ganapathy <aman.ganapathy@telestream.net>
2022-08-29 10:09:37 -0700

    Removed commented code.

--- example-quic-file-transfer/Program.cs
+++ example-quic-file-transfer/Program.cs
@@ -8,9 +8,14 @@ using System.Reactive.Concurrency;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Net;
+enum controlVariables
+{ 
+    BUFFER_SIZE = 1024 * 1024,
+}
 
 public class Program
 {
+    
     public static async Task Main(string[] args)
     {
         var serverTask = Server();
@@ -78,12 +83,14 @@ public class Program
         ImmediateScheduler scheduler = Scheduler.Immediate;
 
         IStopwatch stopwatch = scheduler.StartStopwatch();
-        string endPath = @"C:\src\dir1\output.mxf";
+        string endPath = @"C:\src\dir1\output.mov";
         await ReceiveFile(stream, clientID, stopwatch, endPath);
     }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Interoperability", "CA1416:Validate platform compatibility", Justification = "<Pending>")]
     private static async Task Client()
     {
-        string path = @"C:\src\FCC_17_M10_POR_vs_NZL_UHD_TRIMMED.mxf";
+        string path = @"C:\src\file_example_MOV_480_700kB.mov";
         string conn = "127.0.0.1:5000";
         var options = new QuicClientConnectionOptions
         {
@@ -135,88 +142,15 @@ public class Program
             using FileStream fsSource = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
 
             TimeSpan oldTime = stopwatch.Elapsed;
-            /*do
-            {
-                byte[] msgArr = new byte[1024 * 400];
-
-                //read from file
-                try
-                {
-                    read = await fsSource.ReadAsync(msgArr, 0, 1024 * 400);
-                    totalRead += read;
-                }
-                catch (Exception e)
-                {
-                    System.Console.WriteLine("Hi");
-                }
-
-                //send over quicconnection
-                if (read != 0)
-                {
-                    Throttle(read, ref processed, maxBytesPerSecond, ref stopwatch, ref scheduler);
-                    await stream.WriteAsync(msgArr, 0, read);
-                }
-
-                //if read == 0, then check if open file
-                //set timeout loop
-                else
-                {
-                    System.Console.WriteLine("Entering timeout loop");
-                    while (timeoutSP > 0)
-                    {
-                        //Thread.Sleep(100);
-                        await Task.Delay(100);
-                        timeoutSP -= 100;
-                        long end;
-                        end = new FileInfo(filePath).Length;
-                        System.Console.WriteLine($"file size in timeout is {end}");
-                        if (end > size)
-                        {
-                            System.Console.WriteLine("Open file");
-                            //send new filesize over stream, and change read so loop doesn't end
-                            read = 1;
-                            size = end;
-                            //break when file size is different
-                            break;
-                        }
-
-                    }
-                    timeoutSP = timeout;
-                    await SendMessage(stream, size.ToString());
-                }
-
-                //update signalR
-                if (TimeSpan.Compare(stopwatch.Elapsed.Subtract(oldTime), TimeSpan.FromSeconds(update)) >= 0)
-                {
-                    _ = UpdateProgress(clientID, Status.TRANSFERRING, stopwatch.Elapsed, totalRead);
-                    oldTime = stopwatch.Elapsed;
-                }
-
-
-            } while (read != 0);
-            System.Console.WriteLine($"{totalRead} was read");
-
-            _ = UpdateProgress(clientID, Status.SUCCESS, stopwatch.Elapsed);
-            System.Console.WriteLine("Client: File sent");
-
-            fsSource.Close();
-        }
-        catch (Exception e)
-        {
-            System.Console.Error.WriteLine("Client exception in SendFile:");
-            System.Console.Error.WriteLine(e);
-            _ = UpdateProgress(clientID, Status.ERROR, stopwatch.Elapsed);
-            throw new Exception("Exception in SendFile");
-        }*/
-            byte[] msgArr = new byte[1024 * 1024];
-            while ((read = await fsSource.ReadAsync(msgArr, 0, 1024*1024)) > 0)
+            
+            byte[] msgArr = new byte[(int)controlVariables.BUFFER_SIZE];
+            while ((read = await fsSource.ReadAsync(msgArr, 0, (int)controlVariables.BUFFER_SIZE)) > 0)
             {
                 totalRead += read;
 
-                //Throttle(read, ref processed, maxBytesPerSecond, ref stopwatch, ref scheduler);
                 await stream.WriteAsync(msgArr, 0, read);
                 msgArr = null;
-                msgArr = new byte[1024 * 1024];
+                msgArr = new byte[(int)controlVariables.BUFFER_SIZE];
                 if (TimeSpan.Compare(stopwatch.Elapsed.Subtract(oldTime), TimeSpan.FromSeconds(update)) >= 0)
                 {
                     oldTime = stopwatch.Elapsed;
@@ -238,8 +172,7 @@ public class Program
     }
     private static async Task ReceiveFile(QuicStream stream, double clientID, IStopwatch stopwatch, string outputPath)
     {
-        //CancellationTokenSource cts = new CancellationTokenSource(); 
-        //cts.CancelAfter(1000 * 6);
+        
         // Read message length
         System.Console.WriteLine($"[Server Connection #{clientID}] Reading message length.");
         long len = long.Parse(await ReceiveMessage(stream));
@@ -248,59 +181,16 @@ public class Program
 
         TimeSpan oldTime = stopwatch.Elapsed;
 
-        // TODO make final / const variable that is set from settings?
+        
         double UPDATE_INTERVAL = 1.0;
 
         long read = 0;
 
-        //System.Console.WriteLine($"[Server Connection #{clientID}] Enter read loop.");
         try
         {
-            /*while (len - read != 0)
-            {
-                byte[] buffer = new byte[1024 * 400];
-
-                // Read 1024*1024 bytes unless there aren't enough bytes left
-                int amountRead;
-                if (len - read < 1024 * 400)
-                {
-                    amountRead = (int)(len - read);
-                }
-                else
-                {
-                    amountRead = 1024 * 400;
-                }
-
-                int temp = await stream.ReadAsync(buffer, 0, amountRead);
-
-                read += temp;
-
-                await fsNew.WriteAsync(buffer, 0, temp);
-
-                // Update signalR 
-                if (TimeSpan.Compare(stopwatch.Elapsed.Subtract(oldTime), TimeSpan.FromSeconds(UPDATE_INTERVAL)) >= 0)
-                {
-                    _ = UpdateProgress(clientID, Status.TRANSFERRING, stopwatch.Elapsed, outputPath, read);
-
-                    oldTime = stopwatch.Elapsed;
-                }
-
-
-                // Check if filesize is increasing for open files
-                if (len - read == 0)
-                {
-                    var res = await ReceiveMessage(stream);
-
-                    len = long.Parse(res);
-                }
-            }
-        } catch (Exception e)
-        {
-            System.Console.WriteLine($"[Server Connection #{clientID}] Error while reading file\n{e}");
-        }*/
-            byte[] buffer = new byte[1024*1024];
+            byte[] buffer = new byte[(int)controlVariables.BUFFER_SIZE];
             int temp;
-            while ((temp = await stream.ReadAsync(buffer, 0, 1024*1024)) > 0)
+            while ((temp = await stream.ReadAsync(buffer, 0, (int)controlVariables.BUFFER_SIZE)) > 0)
             {
 
                 await fsNew.WriteAsync(buffer, 0, temp);
@@ -311,7 +201,7 @@ public class Program
 
                     oldTime = stopwatch.Elapsed;
                 }
-                buffer = new byte[1024*1024];
+                buffer = new byte[(int)controlVariables.BUFFER_SIZE];
 
             }
             buffer = null;
